d9b6263514d32e8f9be108557bd1f425d0ed1f3a
mi <61391965+Nekuskus@users.noreply.github.com>
2024-05-03 00:11:01 +0200

    26 current  2024-05-02 23:57:06  23.11.6759.0638fe2715d9  6.1.88                          *

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -23,26 +23,26 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  #   systemd = {
-  #     targets = {
-  #       sleep = {
-  #         enable = false;
-  #         unitConfig.DefaultDependencies = "no";
-  #       };
-  #       suspend = {
-  #         enable = false;
-  #         unitConfig.DefaultDependencies = "no";
-  #       };
-  #       hibernate = {
-  #         enable = false;
-  #         unitConfig.DefaultDependencies = "no";
-  #       };
-  #       "hybrid-sleep" = {
-  #         enable = false;
-  #         unitConfig.DefaultDependencies = "no";
-  #       };
-  #     };
-  #   };
+  systemd = {
+    targets = {
+      sleep = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      suspend = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      hibernate = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      "hybrid-sleep" = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+    };
+  };
 
   systemd.sleep.extraConfig = ''
     AllowSuspend=no
