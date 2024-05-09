8ac0de138e5e5047ebc7f0d71c27c9ab2bc564d5
mi <61391965+Nekuskus@users.noreply.github.com>
2024-05-09 19:22:13 +0200

    36 current  2024-05-09 19:22:09  23.11.6759.0638fe2715d9  6.1.88                          *

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -11,7 +11,7 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd" "snd_pcsp"];
+  boot.kernelModules = ["kvm-amd" "pcspkr"];
   boot.extraModulePackages = [];
 
   #   probably unnecessary
