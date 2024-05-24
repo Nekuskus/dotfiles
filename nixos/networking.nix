3b9d83571c623dbe6c9b52b08ebe57ab8cef1da8
mi <61391965+Nekuskus@users.noreply.github.com>
2024-05-24 11:31:31 +0200

    49 current  2024-05-24 11:31:27  24.05.20240509.f1010e0   6.6.30                          *

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -22,7 +22,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   systemd.user.services.tailscaleCert = {
     description = "...";
