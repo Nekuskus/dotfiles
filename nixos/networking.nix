7c91f376a33d2cdfb7ee262642eee4ed4206c7b7
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-06-08 21:39:08 +0200

    64 current  2024-06-08 21:39:03  24.05.20240509.f1010e0   6.6.30                          *

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -19,7 +19,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [5173];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
