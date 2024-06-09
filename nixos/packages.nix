8909d6e775c6f38dcb3f9391cebac69572fccb93
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-06-09 21:03:56 +0200

    67 current  2024-06-09 21:03:53  24.05.20240509.f1010e0   6.6.30                          *

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -36,6 +36,12 @@
     openFirewall = true;
     capSysAdmin = true;
   };
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
 
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
